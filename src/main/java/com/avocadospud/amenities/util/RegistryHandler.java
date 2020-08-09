8f9aa49c8930343ba04b083d60c3ff79734eb99b
AvocadoSpud <69398022+AvocadoSpud@users.noreply.github.com>
2020-08-09 15:26:06 -0400

    added util

---
+++ src/main/java/com/avocadospud/amenities/util/RegistryHandler.java
@@ -0,0 +1,30 @@
+package com.avocadospud.amenities.util;
+
+import com.avocadospud.amenities.Amenities;
+import com.avocadospud.amenities.blocks.OakTableBlock;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Amenities.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Amenities.MOD_ID);
+
+    public static void init() {
+
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+
+    }
+    // Items
+
+    // Blocks
+    public static final RegistryObject<Block> OAK_TABLE_BLOCK = BLOCKS.register("oak_table", OakTableBlock::new);
+
+    // Block Items
+
+}
